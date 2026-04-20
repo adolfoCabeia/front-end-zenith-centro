6218345e7b2596cf43a415a244a596c1b1de5667
Adolfo Cabeia <adolfojunior544@gmail.com>
2026-04-20 19:26:32 +0100

    up-to-date-20-19-06

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-//process.env.NEXT_PUBLIC_API_URL || 
+
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ||  "http://localhost:8000",
   withCredentials: true,
 });
 
